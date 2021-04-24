5f5943e6db0a77ae9f529339d89c62c6a12c5559
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-04-24 22:08:27 +0100

    make integration test pass

--- book-service/src/main/kotlin/com/artemas/bookservice/web/BookEndpoint.kt
+++ book-service/src/main/kotlin/com/artemas/bookservice/web/BookEndpoint.kt
@@ -1,4 +1,18 @@
 package com.artemas.bookservice.web
 
+import com.artemas.bookservice.domain.Book
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
 class BookEndpoint {
+    @PostMapping("/books/book")
+    @ResponseStatus(CREATED)
+    fun registerBook(@RequestBody book: Book): Mono<Book> {
+        return Mono.just(book)
+    }
 }
