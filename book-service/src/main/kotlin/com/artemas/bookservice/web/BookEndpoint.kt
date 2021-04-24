d05120447e248f854fb4560f0a1b6789656fc456
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-04-24 22:09:34 +0100

    refactor BookEndpoint

--- book-service/src/main/kotlin/com/artemas/bookservice/web/BookEndpoint.kt
+++ book-service/src/main/kotlin/com/artemas/bookservice/web/BookEndpoint.kt
@@ -2,6 +2,7 @@ package com.artemas.bookservice.web
 
 import com.artemas.bookservice.domain.Book
 import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -10,7 +11,8 @@ import reactor.core.publisher.Mono
 
 @RestController
 class BookEndpoint {
-    @PostMapping("/books/book")
+
+    @PostMapping("/books/book", consumes = [APPLICATION_JSON_VALUE])
     @ResponseStatus(CREATED)
     fun registerBook(@RequestBody book: Book): Mono<Book> {
         return Mono.just(book)
