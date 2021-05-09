899a20db10505c98f82b01bd42e0a396a1038a3b
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-09 18:17:45 +0100

    use Reactive MongoRepository

--- book-service/src/main/kotlin/com/artemas/bookservice/repository/BookRepository.kt
+++ book-service/src/main/kotlin/com/artemas/bookservice/repository/BookRepository.kt
@@ -1,8 +1,8 @@
 package com.artemas.bookservice.repository
 
 import com.artemas.bookservice.domain.Book
-import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BookRepository: MongoRepository<Book, Long>
+interface BookRepository: ReactiveMongoRepository<Book, Long>
--- book-service/src/test/kotlin/com/artemas/bookservice/repository/BookRepositoryTest.kt
+++ book-service/src/test/kotlin/com/artemas/bookservice/repository/BookRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.artemas.bookservice.domain.Book
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import reactor.test.StepVerifier
 
 @DataMongoTest
 class BookRepositoryTest(
@@ -13,6 +14,10 @@ class BookRepositoryTest(
     @Test
     fun `Should save new book`() {
         val book = Book(43298784L, 9874L, "Newly Added Book By Arty")
-        bookRepository.save(book)
+
+        StepVerifier.create(bookRepository.save(book))
+            .expectNext(book)
+            .expectComplete()
+            .verify()
     }
 }
