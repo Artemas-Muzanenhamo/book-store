da4ce37138bcc09aef21ac4a2977e3298cc373e1
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-19 19:54:59 +0100

    amend integration tests

--- book-service/src/test/kotlin/com/artemas/bookservice/web/integration/BookEndpointIntegrationTest.kt
+++ book-service/src/test/kotlin/com/artemas/bookservice/web/integration/BookEndpointIntegrationTest.kt
@@ -1,12 +1,16 @@
 package com.artemas.bookservice.web.integration
 
 import com.artemas.bookservice.domain.Book
+import com.artemas.bookservice.repository.BookRepository
 import com.artemas.bookservice.service.BookService
 import com.artemas.bookservice.web.BookEndpoint
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -15,14 +19,19 @@ import reactor.core.publisher.Mono.just
 import reactor.test.StepVerifier
 
 @ExtendWith(SpringExtension::class)
-@WebFluxTest(BookEndpoint::class, BookService::class)
-internal class BookEndpointIntegrationTest(
-    @Autowired val webTestClient: WebTestClient
-) {
+@WebFluxTest(BookEndpoint::class)
+@Import(BookService::class)
+internal class BookEndpointIntegrationTest {
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+    @MockBean
+    lateinit var bookRepository: BookRepository
+
     private val book = Book(4132L, 987434L, "The Great Expectations of Prime")
 
     @Test
     fun `Should Add A Book`() {
+        given(bookRepository.save(book)).willReturn(just(book)) // TODO: Use Real Test DB
 
         val result = webTestClient
             .post()
@@ -44,6 +53,8 @@ internal class BookEndpointIntegrationTest(
 
     @Test
     fun `Should Retrieve A Book Given A Book Id`() {
+        given(bookRepository.findById(book.id)).willReturn(just(book)) // TODO: Use Real Test DB
+
         val result = webTestClient
             .get()
             .uri("/books/${book.id}")
