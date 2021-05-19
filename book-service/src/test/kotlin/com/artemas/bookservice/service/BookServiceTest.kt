69b358d3fc8bc83470375e3222fadfc309dd86e6
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-19 19:30:37 +0100

    fix BookServiceTest

--- book-service/src/test/kotlin/com/artemas/bookservice/service/BookServiceTest.kt
+++ book-service/src/test/kotlin/com/artemas/bookservice/service/BookServiceTest.kt
@@ -4,21 +4,24 @@ import com.artemas.bookservice.domain.Book
 import com.artemas.bookservice.repository.BookRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
+import org.mockito.BDDMockito.given
 import org.mockito.Mock
 import org.mockito.Mockito.verify
 import org.mockito.junit.jupiter.MockitoExtension
+import reactor.core.publisher.Mono.just
 import reactor.test.StepVerifier
 
 @ExtendWith(MockitoExtension::class)
 class BookServiceTest(
-    @InjectMocks val bookService: BookService,
     @Mock val bookRepository: BookRepository
 ) {
+    private val bookService = BookService(bookRepository)
     private val book = Book(789234L, 44333L, "Some Book Title Here")
 
     @Test
     fun `Should create a book`() {
+        given(bookRepository.save(book)).willReturn(just(book))
+
         StepVerifier.create(bookService.addBook(book))
             .expectNext(book)
             .expectComplete()
@@ -29,6 +32,8 @@ class BookServiceTest(
 
     @Test
     fun `Should retrieve a book by id`() {
+        given(bookRepository.findById(book.id)).willReturn(just(book))
+
         StepVerifier.create(bookService.getBookById(book.id))
             .expectNext(book)
             .expectComplete()
