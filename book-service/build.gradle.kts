6b4e680dbd0d8b7a87e1c829d893cdb690a1f924
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-21 22:04:44 +0100

    amend build gradle

--- book-service/build.gradle.kts
+++ book-service/build.gradle.kts
@@ -38,7 +38,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.0.0")
+//    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.0.0")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo.download-and-extract:2.0.0")
 }
 
 dependencyManagement {
