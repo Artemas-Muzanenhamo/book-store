4984a5bc5ed698fd5ca7e4be9ff80fd21cbd07c8
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-30 09:12:08 +0100

    Add pact directory

--- author-service/src/test/kotlin/com/artemas/authorservice/end-to-end/CreateAuthorPactTest.kt
+++ author-service/src/test/kotlin/com/artemas/authorservice/end-to-end/CreateAuthorPactTest.kt
@@ -7,6 +7,8 @@ import au.com.dius.pact.consumer.junit5.PactConsumerTestExt
 import au.com.dius.pact.consumer.junit5.PactTestFor
 import au.com.dius.pact.core.model.RequestResponsePact
 import au.com.dius.pact.core.model.annotations.Pact
+import au.com.dius.pact.core.model.annotations.PactDirectory
+import au.com.dius.pact.core.model.annotations.PactFolder
 import au.com.dius.pact.core.support.hasProperty
 import au.com.dius.pact.core.support.jsonObject
 import au.com.dius.pact.core.support.property
@@ -30,6 +32,7 @@ import java.io.IOException
 
 @ExtendWith(PactConsumerTestExt::class)
 @PactTestFor(providerName = "book-service")
+@PactDirectory("../pacts")
 class CreateAuthorPactTest {
 
     val json: PactDslJsonBody = PactDslJsonBody()
